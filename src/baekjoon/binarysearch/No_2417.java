a9e1ecffd6672962fb123e7c7e5dd809885d6895
JayMaro <jinkyu1996@hanmail.net>
2023-08-28 22:45:43 +0900

    Solved baekjoon No_2417 2

--- src/baekjoon/binarysearch/No_2417.java
+++ src/baekjoon/binarysearch/No_2417.java
@@ -1,5 +1,15 @@
 package baekjoon.binarysearch;
 
-public class No_2417 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class No_2417 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        long number = Long.parseLong(br.readLine());
+        double ans = Math.floor(Math.ceil(Math.sqrt(number)));
+        System.out.printf("%.0f", ans);
+    }
 }
