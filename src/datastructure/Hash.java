5fa400579d010ab958f20e41baa8398bfc892b44
JayMaro <jinkyu1996@hanmail.net>
2024-02-24 18:18:54 +0900

    Add DataStructure Hash comment

---
+++ src/datastructure/Hash.java
@@ -0,0 +1,26 @@
+package datastructure;
+
+public class Hash {
+    /**
+     * add -> ?
+     * remove -> ?
+     * lookup/find -> ?
+     * change -> ?
+     *
+     * all entries -> n
+     * all keys -> n
+     * all value -> n
+     *
+     * size -> O(n)
+     * isEmpty -> O(n)
+     * isFull -> O(n)
+     * loadFactor -> O(n)
+     *
+     * Associative array
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
