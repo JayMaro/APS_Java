ae8fce65f581aebff294428cffbb9e27afa0023e
JayMaro <jinkyu1996@hanmail.net>
2024-02-26 00:28:10 +0900

    Modify DataStructure Hash comment

--- src/datastructure/Hash.java
+++ src/datastructure/Hash.java
@@ -1,24 +1,18 @@
 package datastructure;
 
+/**
+ * Associative array
+ *
+ * - property of the data
+ * - be fast to compute
+ * - If two things are "equal" should return the same value
+ * - Always return the same value for an object during one run of the code
+ * - possible to return different values for an object in separate runs
+ *   - not at disk but at Object hash code by memory address
+ * - *minimize collisions*
+ *
+ */
 public class Hash {
-    /**
-     * add -> ?
-     * remove -> ?
-     * lookup/find -> ?
-     * change -> ?
-     *
-     * all entries -> n
-     * all keys -> n
-     * all value -> n
-     *
-     * size -> O(n)
-     * isEmpty -> O(n)
-     * isFull -> O(n)
-     * loadFactor -> O(n)
-     *
-     * Associative array
-     *
-     */
 
     public static void main(String[] args) {
 
