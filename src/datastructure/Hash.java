02a0411fb90262456df2dab513647e783968d706
JayMaro <jinkyu1996@hanmail.net>
2024-02-27 03:24:25 +0900

    Add: hashCode function to Hash

--- src/datastructure/Hash.java
+++ src/datastructure/Hash.java
@@ -15,6 +15,16 @@ package datastructure;
 public class Hash {
 
     public static void main(String[] args) {
+        System.out.println(hashCode("this"));
+        System.out.println(hashCode("shit"));
+    }
 
+    public static int hashCode(String s) {
+        int g = 31;
+        int hash = 0;
+        for (int i = 0; i < s.length(); i++) {
+            hash = g * hash + s.charAt(i);
+        }
+        return hash;
     }
 }
