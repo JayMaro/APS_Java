5e9986b84e13b6c0f93488bb6b5b0671a69b3712
JayMaro <jinkyu1996@hanmail.net>
2024-03-06 23:55:15 +0900

    Add No_70

---
+++ src/leetcode/problem/No_70.java
@@ -0,0 +1,26 @@
+package leetcode.problem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_70 {
+
+    public static void main(String[] args) {
+        climbStairs(0);
+    }
+
+    public static int climbStairs(int n) {
+        int sum = 0;
+        List<String> resultList = new ArrayList<>();
+        String test = "";
+        test += 1;
+        test += 2;
+        System.out.println(test);
+        test = test.substring(0, test.length()-1);
+        System.out.println(test);
+//       while (true) {
+//
+//       }
+        return 0;
+    }
+}
