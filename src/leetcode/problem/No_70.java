8a9eaba26230d7e7cd7bef61662a758177b7b4b1
JayMaro <jinkyu1996@hanmail.net>
2024-03-07 00:28:20 +0900

    Add No_70 2

--- src/leetcode/problem/No_70.java
+++ src/leetcode/problem/No_70.java
@@ -1,26 +1,39 @@
 package leetcode.problem;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class No_70 {
 
     public static void main(String[] args) {
-        climbStairs(0);
+        System.out.println(climbStairs(35));
     }
 
+
+
     public static int climbStairs(int n) {
-        int sum = 0;
-        List<String> resultList = new ArrayList<>();
-        String test = "";
-        test += 1;
-        test += 2;
-        System.out.println(test);
-        test = test.substring(0, test.length()-1);
-        System.out.println(test);
-//       while (true) {
-//
-//       }
-        return 0;
+        LinkedList<Object[]> queue = new LinkedList<>();
+        Set<String> resultList = new HashSet<>();
+        Set<String> reqList = new HashSet<>();
+        queue.add(new Object[]{0, ""});
+        while (queue.size() != 0) {
+            Object[] valueArray = queue.removeFirst();
+            if ((Integer) valueArray[0] > n) {
+                continue;
+            }
+            if (reqList.contains((String) valueArray[1])) {
+                continue;
+            }
+            reqList.add((String) valueArray[1]);
+            if ((Integer) valueArray[0] == n) {
+                resultList.add((String) valueArray[1]);
+            }
+            queue.addLast(new Object[]{(Integer) valueArray[0] + 2, (String) valueArray[1] + 2});
+            queue.addLast(new Object[]{(Integer) valueArray[0] + 1, (String) valueArray[1] + 1});
+        }
+        return resultList.size();
     }
 }
