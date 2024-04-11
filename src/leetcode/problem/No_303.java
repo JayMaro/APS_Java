3d72b7eded7c17f70b52a066340ae21ae56392f6
JayMaro <jinkyu1996@hanmail.net>
2024-04-11 15:49:28 +0900

    LeetCode No_303 developed answer

--- src/leetcode/problem/No_303.java
+++ src/leetcode/problem/No_303.java
@@ -25,6 +25,30 @@ public class No_303 {
         System.out.println(obj.sumRange(2,5));
         System.out.println(obj.sumRange(0,5));
 
+        class NumArray2 {
+
+            int[] sums;
+            int[] nums;
+
+            public NumArray2(int[] nums) {
+                this.nums = nums;
+                this.sums = new int[nums.length];
+                this.sums[0] = nums[0];
+
+                for (int i=1; i<nums.length; i++){
+                    this.sums[i] = this.sums[i-1] + nums[i];
+                }
+            }
+
+            public int sumRange(int left, int right) {
+                return this.sums[right] - this.sums[left] + this.nums[left];
+            }
+        }
+
+        NumArray2 obj2 = new NumArray2(new int[]{-2, 0, 3, -5, 2, -1});
+        System.out.println(obj2.sumRange(0,2));
+        System.out.println(obj2.sumRange(2,5));
+        System.out.println(obj2.sumRange(0,5));
 
     }
 
