f11e2c2e0c7d335a72157c745e55cb1d19617952
JayMaro <jinkyu1996@hanmail.net>
2024-04-15 22:50:02 +0900

    Heaps 1 Introduction and Tree levels

---
+++ src/datastructure/TreesAndHeap.java
@@ -0,0 +1,25 @@
+package datastructure;
+
+/*
+ * root / leaves
+ * parent / child
+ * siblings
+ * node(점) / edge(선)
+ * levels root(1) / child(2)
+ */
+public class TreesAndHeap {
+
+}
+
+class Tree {
+
+}
+
+class Heap {
+    /*
+    parent is > children -> max heap
+    parent is < children -> min heap
+    max children is 2
+    level(height) = log2(n+1)-1
+     */
+}
