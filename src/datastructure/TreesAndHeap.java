074f5a252219f30fc645f2997b14b6e46fdd8ae0
JayMaro <jinkyu1996@hanmail.net>
2024-04-16 21:20:40 +0900

    Heaps 2 Add Remove

--- src/datastructure/TreesAndHeap.java
+++ src/datastructure/TreesAndHeap.java
@@ -21,5 +21,17 @@ class Heap {
     parent is < children -> min heap
     max children is 2
     level(height) = log2(n+1)-1
+
+    add
+     -> 빈 자리에 더하고 부모와 비교 후 스왑
+        -> insert next available space
+        -> trickle up
+    remove
+    -> 항상 최상단의 root를 제거해야 한다.
+    -> 가장 마지막 노드를 최상단으로 옮긴다.
+    -> 자식과 비교하며 스왑한다. (이 때 둘중 큰 노드와 스왑한다.)
+        -> remove the root(Always!)
+        -> replace with the last element
+        -> trickle down(compare two and swap with bigger one)
      */
 }
