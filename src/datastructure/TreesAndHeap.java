0b2d12d1275b0dc6058b29746b0f58b8b09148a6
JayMaro <jinkyu1996@hanmail.net>
2024-04-17 22:00:57 +0900

    Heaps 3 TrickleUp

--- src/datastructure/TreesAndHeap.java
+++ src/datastructure/TreesAndHeap.java
@@ -12,6 +12,7 @@ public class TreesAndHeap {
 }
 
 class Tree {
+    // Tree Traversal
 
 }
 
@@ -33,5 +34,35 @@ class Heap {
         -> remove the root(Always!)
         -> replace with the last element
         -> trickle down(compare two and swap with bigger one)
+
+    Index
+    - children index: 2*parent+1, 2*parent+2
+    - parent index: floor((child-1)/2)
+    -> 이를 통해 Heap을 간단하게 Array로 구현할 수 있다.
+
      */
+    class BinaryHeapImpl<E> {
+        int lastPosition;   // how many element we have
+        E[] array; // = (E[]) new Object[size]
+
+        public void add(E obj) {
+            array[++lastPosition] = obj;
+             trickleUp(lastPosition);
+        }
+        public void swap(int from, int to) {
+            E tmp = array[from];
+            array[from] = array[to];
+            array[to] = tmp;
+        }
+        public void trickleUp(int position) {
+            if (position == 0) {
+                return;
+            }
+            int parent = (int) Math.floor((position-1)/2);
+            if (((Comparable<E>)array[position]).compareTo(array[parent]) > 0) {
+                swap(position, parent);
+                trickleUp(parent);
+            }
+        }
+    }
 }
