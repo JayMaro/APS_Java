db95d7978fd678ef48e9061fd5f15ec6480ca7d6
JayMaro <jinkyu1996@hanmail.net>
2024-04-18 18:47:42 +0900

    Heaps 4 TrickleDown

--- src/datastructure/TreesAndHeap.java
+++ src/datastructure/TreesAndHeap.java
@@ -17,6 +17,7 @@ class Tree {
 }
 
 class Heap {
+
     /*
     parent is > children -> max heap
     parent is < children -> min heap
@@ -42,27 +43,62 @@ class Heap {
 
      */
     class BinaryHeapImpl<E> {
+
         int lastPosition;   // how many element we have
         E[] array; // = (E[]) new Object[size]
 
         public void add(E obj) {
             array[++lastPosition] = obj;
-             trickleUp(lastPosition);
+            trickleUp(lastPosition);
         }
+
         public void swap(int from, int to) {
             E tmp = array[from];
             array[from] = array[to];
             array[to] = tmp;
         }
+
         public void trickleUp(int position) {
             if (position == 0) {
                 return;
             }
-            int parent = (int) Math.floor((position-1)/2);
-            if (((Comparable<E>)array[position]).compareTo(array[parent]) > 0) {
+            int parent = (int) Math.floor((position - 1) / 2);
+            if (((Comparable<E>) array[position]).compareTo(array[parent]) > 0) {
                 swap(position, parent);
                 trickleUp(parent);
             }
+        }
+
+        public E remove() {
+            E tmp = array[0];
+            swap(0, lastPosition--);
+            trickleDown(0);
+            return tmp;
+        }
+
+        public void trickleDown(int parent) {
+            int left = 2 * parent + 1;
+            int right = 2 * parent + 2;
+            if (left == lastPosition &&
+                (((Comparable<E>) array[parent]).compareTo(array[left]) < 0)) {
+                    swap(parent, left);
+                    return;
+            }
+            if (right == lastPosition &&
+                (((Comparable<E>) array[parent]).compareTo(array[right]) < 0)) {
+                swap(parent, right);
+                return;
+            }
+            if (left >= lastPosition || right >= lastPosition) {
+                return;
+            }
+            if ((((Comparable<E>) array[left]).compareTo(array[right]) > 0) &&
+                (((Comparable<E>) array[parent]).compareTo(array[left]) > 0)) {
+                swap(parent, left);
+                trickleDown(left);
+            } else if ((((Comparable<E>) array[parent]).compareTo(array[right]) < 0)) {
+                swap(parent, right);
+                trickleDown(right);
         }
     }
 }
