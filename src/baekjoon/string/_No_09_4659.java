10ab184353b36beb43ee935e994a08c1840378fc
JayMaro <jinkyu1996@hanmail.net>
2024-07-11 23:09:31 +0900

    baekjoon.string _No_09_4659

--- src/baekjoon/string/_No_09_4659.java
+++ src/baekjoon/string/_No_09_4659.java
@@ -5,20 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _No_09_4659 {
-
-    static char[] vowels = new char[]{'a', 'e', 'o', 'u', 'i'};
-
+    // a e i o u 하나를 포함
+    // 모음이 3개 or 자음이 3개 연속 x
+    // 같은 글자가 연속 두번은 안되나 ee와 oo는 허용
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        // a e i o u 하나를 포함
-        // 모음이 3개 or 자음이 3개 연속 x -> 같은게 세개 연속으로 오면 안됨
-        // 같은 글자가 연속 두번은 안되나 ee와 oo는 허용
         while (true) {
             String input = br.readLine();
             if ("end".equals(input)) {
                 break;
             }
+
             boolean isContainVowel = false;
             int beforeCharCount = 0;
             int vowelCount = 0;
@@ -26,11 +24,11 @@ public class _No_09_4659 {
             char beforeChar = ' ';
             boolean isAvailable = true;
             char[] chars = input.toCharArray();
+
             for (char c : chars) {
-                if (!isContainVowel) {
-                    isContainVowel = checkIsContain(c);
-                }
-                if (checkIsContain(c)) {
+                // 모음이 3개 or 자음이 3개 연속 x
+                if (checkIsVowel(c)) {
+                    isContainVowel = true; // a e i o u 하나를 포함
                     vowelCount++;
                     consonantCount = 0;
                     if (vowelCount >= 3) {
@@ -45,6 +43,7 @@ public class _No_09_4659 {
                         break;
                     }
                 }
+                // 같은 글자가 연속 두번은 안되나 ee와 oo는 허용
                 if (c == beforeChar) {
                     beforeCharCount++;
                     if (beforeCharCount >= 2 && (beforeChar != 'e' && beforeChar != 'o')) {
@@ -56,12 +55,14 @@ public class _No_09_4659 {
                     beforeCharCount = 1;
                 }
             }
-            sb.append("<").append(input).append(">").append(" is ").append(isContainVowel && isAvailable ? "acceptable." : "not acceptable.").append("\n");
+            sb.append("<").append(input).append(">").append(" is ")
+                .append(isContainVowel && isAvailable ? "acceptable." : "not acceptable.").append("\n");
         }
         System.out.println(sb);
     }
 
-    private static boolean checkIsContain(char c) {
+    private static boolean checkIsVowel(char c) {
+        char[] vowels = new char[]{'a', 'e', 'o', 'u', 'i'};
         for (char vowel : vowels) {
             if (vowel == c) {
                 return true;
