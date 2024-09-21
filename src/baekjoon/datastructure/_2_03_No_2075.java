555630f1ae721da75f48fd49f131baed6d2a8418
JayMaro <jinkyu1996@hanmail.net>
2024-09-21 23:19:58 +0900

    baekjoon.datastructure _2_03_No_2075

--- src/baekjoon/datastructure/_2_03_No_2075.java
+++ src/baekjoon/datastructure/_2_03_No_2075.java
@@ -3,21 +3,24 @@ package baekjoon.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.PriorityQueue;
 
 public class _2_03_No_2075 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
         int n = Integer.parseInt(br.readLine());
-        ArrayList<List<Integer>> arrayLists = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            List<Integer> collect = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-            arrayLists.add(collect);
+            String[] s = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                queue.add(Integer.parseInt(s[j]));
+            }
         }
-
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = queue.poll();
+        }
+        System.out.println(result);
     }
 }
