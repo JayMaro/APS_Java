208bc9eeb10b03443fea09f28608c6eb4b12ff04
JayMaro <jinkyu1996@hanmail.net>
2024-09-25 22:20:58 +0900

    baekjoon.datastructure _2_05_No_7662

--- src/baekjoon/datastructure/_2_05_No_7662.java
+++ src/baekjoon/datastructure/_2_05_No_7662.java
@@ -10,27 +10,31 @@ public class _2_05_No_7662 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        TreeMap<Integer, Integer> map = new TreeMap<>((a,b) -> b - a);
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             int k = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> map = new TreeMap<>((a,b) -> b - a);
             for (int j = 0; j < k; j++) {
                 String[] s = br.readLine().split(" ");
                 if ("I".equals(s[0])) {
                     int val = Integer.parseInt(s[1]);
-                    map.put(val, val);
-                } else if ("1".equals(s[1])) {
-                    if (map.isEmpty()) {
-                        continue;
-                    }
-                    Integer maxVal = map.firstKey();
-                    map.remove(maxVal);
+                    map.put(val, map.getOrDefault(val, 0) + 1);
                 } else {
                     if (map.isEmpty()) {
                         continue;
                     }
-                    Integer minVal = map.lastKey();
-                    map.remove(minVal);
+                    Integer val;
+                    if ("1".equals(s[1])) {
+                        val = map.firstKey();
+                    } else {
+                        val = map.lastKey();
+                    }
+                    Integer count = map.getOrDefault(val, 0);
+                    if (count == 1) {
+                        map.remove(val);
+                    } else if (count != 0) {
+                        map.put(val, count - 1);
+                    }
                 }
             }
             if (map.isEmpty()) {
