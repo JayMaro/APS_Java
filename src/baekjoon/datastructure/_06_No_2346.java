5cca3665a7d51de3801b1680bff533989a16013b
JayMaro <jinkyu1996@hanmail.net>
2024-09-27 00:10:26 +0900

    baekjoon.datastructure _06_No_2346 ver2

--- src/baekjoon/datastructure/_06_No_2346.java
+++ src/baekjoon/datastructure/_06_No_2346.java
@@ -3,6 +3,7 @@ package baekjoon.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,46 +12,38 @@ import java.util.stream.Collectors;
 public class _06_No_2346 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        LinkedList<Balloon> list = new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        ArrayList<Integer> resultList = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         int moveCount = 0;
         int n = Integer.parseInt(br.readLine());
         List<Integer> moveList = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
         for (int i = 0; i < n; i++) {
-            list.add(new Balloon(i + 1, moveList.get(i)));
+            list.add(i);
         }
-        Balloon balloon1 = list.removeFirst();
-        moveCount = balloon1.getMove();
-        while (moveCount != 0) {
+        list.removeFirst();
+        resultList.add(0);
+        moveCount = moveList.get(0);
+        while (!list.isEmpty()) {
             if (moveCount > 0) {
-                Balloon balloon = list.removeFirst();
-                list.addLast(balloon);
                 moveCount -= 1;
-            } else {
-                Balloon balloon = list.removeLast();
-                list.addFirst(balloon);
-                moveCount += 1;
             }
+            while (moveCount != 0) {
+                if (moveCount > 0) {
+                    list.addLast(list.removeFirst());
+                    moveCount -= 1;
+                } else {
+                    list.addFirst(list.removeLast());
+                    moveCount += 1;
+                }
+            }
+            Integer num = list.removeFirst();
+            moveCount = moveList.get(num);
+            resultList.add(num);
         }
-
-
-
-    }
-
-    public static class Balloon {
-        private int num;
-        private int move;
-
-        public Balloon(int num, int move) {
-            this.num = num;
-            this.move = move;
+        for (Integer num : resultList) {
+            sb.append(num+1).append(" ");
         }
-
-        public int getNum() {
-            return this.num;
-        }
-
-        public int getMove() {
-            return this.move;
-        }
+        System.out.println(sb);
     }
 }
