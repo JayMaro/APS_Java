c2de76782a42948f633cf6240caed11026e4d4d0
JayMaro <jinkyu1996@hanmail.net>
2024-09-29 00:05:47 +0900

    baekjoon.datastructure _07_No_1966

--- src/baekjoon/datastructure/_07_No_1966.java
+++ src/baekjoon/datastructure/_07_No_1966.java
@@ -3,6 +3,8 @@ package baekjoon.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class _07_No_1966 {
@@ -12,20 +14,29 @@ public class _07_No_1966 {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
-            PriorityQueue<Doc> docPriorityQueue = new PriorityQueue<>(Doc::compareTo);
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+            ArrayDeque<Doc> docArrayDeque = new ArrayDeque<>();
             String[] s = br.readLine().split(" ");
             int number = Integer.parseInt(s[0]);
             int purposeIndex = Integer.parseInt(s[1]);
             int orderNum = 0;
             String[] priorityArray = br.readLine().split(" ");
             for (int j = 0; j < number; j++) {
-                docPriorityQueue.add(new Doc(j, Integer.parseInt(priorityArray[j]), purposeIndex == j));
+                int priority = Integer.parseInt(priorityArray[j]);
+                docArrayDeque.addLast(new Doc(priority, purposeIndex == j));
+                priorityQueue.add(priority);
             }
+            Integer priority = priorityQueue.poll();
             while (true) {
-                Doc doc = docPriorityQueue.poll();
-                orderNum++;
-                if (doc.isPurpose) {
-                    break;
+                Doc doc = docArrayDeque.removeFirst();
+                if (doc.getPriority() == priority) {
+                    orderNum++;
+                    if (doc.isPurpose()) {
+                        break;
+                    }
+                    priority = priorityQueue.poll();
+                } else {
+                    docArrayDeque.addLast(doc);
                 }
             }
             sb.append(orderNum).append("\n");
@@ -33,19 +44,13 @@ public class _07_No_1966 {
         System.out.println(sb);
     }
 
-    public static class Doc implements Comparable{
-        private int index;
+    public static class Doc{
         private int priority;
         private boolean isPurpose;
 
-        public Doc(int index, int priority, boolean isPurpose) {
-            this.index = index;
+        public Doc(int priority, boolean isPurpose) {
             this.priority = priority;
             this.isPurpose = isPurpose;
-        }
-
-        public int getIndex() {
-            return index;
         }
 
         public int getPriority() {
@@ -54,15 +59,6 @@ public class _07_No_1966 {
 
         public boolean isPurpose() {
             return isPurpose;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            if (o instanceof Doc) {
-                return ((Doc) o).getPriority() - this.getPriority();
-            } else {
-                throw new IllegalArgumentException();
-            }
         }
     }
 }
