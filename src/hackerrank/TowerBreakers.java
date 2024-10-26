a899205f1a9ba4171d56d9fd72f8dd97a1448d40
JayMaro <jinkyu1996@hanmail.net>
2024-10-26 23:38:39 +0900

    hackerrank TowerBreakers

---
+++ src/hackerrank/TowerBreakers.java
@@ -0,0 +1,48 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class TowerBreakers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = Result.towerBreakers(n, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static class Result {
+
+        public static int towerBreakers(int n, int m) {
+            if (m == 1 || n % 2 == 0) {
+                return 2;
+            } else {
+                return 1;
+            }
+        }
+
+    }
+}
